42cce6d55f4012485bcb9d931a3007e3f945dffd
zozrom <zozulia.roman135@gmail.com>
2018-12-17 21:51:55 +0200

    Добавьте файлы проекта.

---
+++ Project1/Source.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	 
+	cout << "matrix B of dimension n × m. Find the maximum and minimum elements in each row of the matrix and swap them with the first and last elements of the row, respectively.";
+
+		 
+	return 0;
+
+ }
